9ed12c723afb6b56630a4154a43c5bc8fe95a0aa
mansibhagoria <mansibhagoria@gmail.com>
2024-03-18 16:08:15 +0530

    Dashboard

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import CustomerList from "@/components/CustomerList/CustomerList";
+import Dashboard from "@/components/Dashboard/Dashboard";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Dashboard/>
+  );
+}
